b525df22ef9f3c30c508d993579975411940d1d0
dovlin_m <dovlin.m@gmail.com>
2022-08-08 13:30:13 +0300

    add axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import vuetify from './plugins/vuetify'
+import VueAxios from './plugins/axios'
+import vueCustomElement from 'vue-custom-element'
+
+import 'document-register-element/build/document-register-element'
+
+
+Vue.use(vueCustomElement);
+Vue.use(VueAxios)
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
+Vue.customElement('weather-widget', App)
